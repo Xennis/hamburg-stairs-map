82e020b0ef47271c8a94f7e28f5bc209e71b3f6a
Xennis <code@xennis.org>
2021-10-10 22:23:17 +0200

    App | Fix osm links. Add color for sac scale

--- apps/steps-map/src/components/StepsMap.vue
+++ apps/steps-map/src/components/StepsMap.vue
@@ -1,7 +1,7 @@
 <template>
   <l-map @ready="onReady" style="position: absolute; top: 0; bottom: 0; width: 100%;" :zoom="zoom" :center="center">
     <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-    <l-geo-json :geojson="stepsData" :options="geojsonOptions"></l-geo-json>
+    <l-geo-json :geojson="stepsData" :options="stepsDataOptions"></l-geo-json>
   </l-map>
 </template>
 
@@ -31,9 +31,8 @@ export default {
         '&copy; <a href="https://www.mapbox.com/about/maps/" target="_blank">Mapbox</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors | <a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a>',
       zoom: 15,
       center: [53.551086, 9.993682],
-
       map: null,
-      geojsonOptions: {
+      stepsDataOptions: {
         onEachFeature: function onEachFeature(feature, layer) {
           var text = '';
           if (feature.properties.name) {
@@ -46,17 +45,19 @@ export default {
             }
             text += `${key}: ${value}\n`;
           }
-          text += '</pre><p>Data: <a href="https://www.openstreetmap.org/" + feature.properties["@id"] + " target="_blank">OpenStreetMap</a> / <a href="https://opendatacommons.org/licenses/odbl/1-0/" target="_blank">ODbL</a></p>'
+          text += '</pre><p>Data: <a href="https://www.openstreetmap.org/' + feature.properties["@id"] + '" target="_blank">OpenStreetMap</a> / <a href="https://opendatacommons.org/licenses/odbl/1-0/" target="_blank">ODbL</a></p>'
           layer.bindPopup(text);
         },
         style: function(feature) {
-          if (feature.properties.wikipedia) {
+          if (feature.properties.sac_scale) {
             return {
-              weight: 6,
-              color: '#03fcb1'
+              color: '#F4941B'
             }
           }
         },
+        //filter: function(feature) {
+        //  return feature.properties.handrail == 'no';
+        //},
       }
     };
   },
